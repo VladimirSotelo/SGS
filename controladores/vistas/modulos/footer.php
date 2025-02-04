29c5962f18392ac707b815ff95dd97488416709c
VladimirSotelo <vladysotelo0088@gmail.com>
2025-02-04 08:20:11 -0300

    correccion

--- controladores/vistas/modulos/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col fs-13 text-muted text-center">
-                &copy; <script>document.write(new Date().getFullYear())</script> - Hecho con <span class="mdi mdi-heart text-danger"></span>  <a href="#!" class="text-reset fw-semibold"> </a> 
-            </div>
-        </div>
-    </div>
-</footer>
